1de4a79290d0bf1c929bf95fdebca64438919761
eyg0605 <929447444@qq.com>
2020-10-20 22:42:52 +0900

    mybatisをより深く理解するために、実行の流れを分析し、mavenでmybatisを依頼しないで、mybatisを真似する

---
+++ 01_04mybatis_design_bymyself/src/main/java/com/itstudy/mybatis/cfg/Mapper.java
@@ -0,0 +1,24 @@
+package com.itstudy.mybatis.cfg;
+
+//ｓｑｌとresultType の全修飾クラス名をパッケージするためのクラス
+public class Mapper {
+
+    private String queryString;
+    private String resultType;
+
+    public String getQueryString() {
+        return queryString;
+    }
+
+    public void setQueryString(String queryString) {
+        this.queryString = queryString;
+    }
+
+    public String getResultType() {
+        return resultType;
+    }
+
+    public void setResultType(String resultType) {
+        this.resultType = resultType;
+    }
+}
