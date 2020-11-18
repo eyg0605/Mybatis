909533991b8ea2c8933f9e9af2ad0ca8bcac4e43
eyg0605 <929447444@qq.com>
2020-11-18 22:26:49 +0900

    userと role　は　many to many

---
+++ 03_04manytomany/src/main/java/com/itstudy/dao/RoleDao.java
@@ -0,0 +1,10 @@
+package com.itstudy.dao;
+
+import com.itstudy.domain.Role;
+
+import java.util.List;
+
+public interface RoleDao {
+
+    List<Role> findAll();
+}
---
+++ 03_04manytomany/src/test/java/com/itstudy/test/RoleTest.java
@@ -0,0 +1,44 @@
+package com.itstudy.test;
+
+import com.itstudy.dao.RoleDao;
+import com.itstudy.dao.UserDao;
+import com.itstudy.domain.Role;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class RoleTest {
+
+    private InputStream in;
+    private SqlSessionFactory factory;
+    private SqlSession session;
+    private RoleDao roleDao;
+
+    @Before
+    public void init() throws Exception {
+        in = Resources.getResourceAsStream("SqlMapConfig.xml");
+        factory=new SqlSessionFactoryBuilder().build(in);
+        session=factory.openSession(true);
+        roleDao= session.getMapper(RoleDao.class);
+    }
+    @After
+    public void destory() throws Exception {
+        session.close();
+        in.close();
+    }
+    @Test
+    public void testFindAll(){
+        List<Role> roles = roleDao.findAll();
+        for (Role role : roles) {
+            System.out.println(role);
+            System.out.println(role.getUsers());
+        }
+    }
+}
